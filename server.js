53d6a6b99b9970cba1a3ad0ebb99ccce5be5fb51
kakipi16 <kakipi533@gmail.com>
2023-07-12 18:19:08 +0900

    seconde commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const PORT = 8000;
+const html = require("fs").readFileSync("./index.html")
+
+// web サーバーを作ろう
+const server = http.createServer((req, res) => {
+    // ブラウザからアクセスが来た時の処理
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(html);
+    res.end();
+    if (req.method == "GET") {
+    }
+    if (req.method == "POST") {
+    }
+});
+
+server.listen(PORT, () => {
+    console.log("server running!")
+});
